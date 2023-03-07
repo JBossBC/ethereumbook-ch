917984287b978b07ddabf92c1e72c0717fecb0df
xiyang <jbossbc@gmail.com>
2023-03-07 15:35:37 +0800

    finish three page

---
+++ book/code/Solidity/Faucet.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: CC-BY-SA-4.0
+
+pragma solidity ^0.8.0;
+
+
+contract Faucet{
+    receive()external payable{}
+    function withdraw(uint withdraw_amount)public{
+        require(withdraw_amount<100000000000000000);
+        msg.sender.transfer(withdraw_amount);
+    }
+}
